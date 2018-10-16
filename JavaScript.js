7a7514cdc4b662ecccbc0aa6806653e897e8d3a0
unknown <reddyswathi590@gmail.com>
2018-10-16 21:06:14 +0530

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+﻿function Customer(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
